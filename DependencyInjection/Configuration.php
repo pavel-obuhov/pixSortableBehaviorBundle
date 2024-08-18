22c0f8ea1885cfdb4fc5f5db683edd0e5c37f25f
Pavel Obuhov <pavel.obuhov@gmail.com>
2024-08-18 20:30:54 +0300

    Symfony7 fix

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -10,64 +10,54 @@
 
 namespace Pix\SortableBehaviorBundle\DependencyInjection;
 
-use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 
-/**
- * This is the class that validates and merges configuration from your app/config files
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
- */
 class Configuration implements ConfigurationInterface
 {
-    /**
-     * {@inheritDoc}
-     */
     public function getConfigTreeBuilder()
     {
         $supportedDrivers = array('orm', 'mongodb');
 
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('pix_sortable_behavior');
+        $treeBuilder = new TreeBuilder('pix_sortable_behavior');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
             ->scalarNode('db_driver')
-                ->info(sprintf(
-                    'These following drivers are supported: %s',
-                    implode(', ', $supportedDrivers)
-                ))
-                ->validate()
-                    ->ifNotInArray($supportedDrivers)
-                    ->thenInvalid('The driver "%s" is not supported. Please choose one of ('.implode(', ', $supportedDrivers).')')
-                ->end()
-                ->cannotBeOverwritten()
-                ->cannotBeEmpty()
-                ->defaultValue('orm')
+            ->info(sprintf(
+                'These following drivers are supported: %s',
+                implode(', ', $supportedDrivers)
+            ))
+            ->validate()
+            ->ifNotInArray($supportedDrivers)
+            ->thenInvalid('The driver "%s" is not supported. Please choose one of ('.implode(', ', $supportedDrivers).')')
+            ->end()
+            ->cannotBeOverwritten()
+            ->cannotBeEmpty()
+            ->defaultValue('orm')
             ->end()
             ->arrayNode('position_field')
-                ->addDefaultsIfNotSet()
-                ->children()
-                    ->scalarNode('default')
-                        ->defaultValue('position')
-                    ->end()
-                    ->arrayNode('entities')
-                        ->prototype('scalar')->end()
-                    ->end()
-                ->end()
+            ->addDefaultsIfNotSet()
+            ->children()
+            ->scalarNode('default')
+            ->defaultValue('position')
             ->end()
-
+            ->arrayNode('entities')
+            ->prototype('scalar')->end()
+            ->end()
+            ->end()
+            ->end()
             ->arrayNode('sortable_groups')
-                ->addDefaultsIfNotSet()
-                ->children()
-                    ->arrayNode('entities')
-                        ->useAttributeAsKey('name')
-                        ->prototype('variable')
-                        ->end()
-                    ->end()
-                ->end()
+            ->addDefaultsIfNotSet()
+            ->children()
+            ->arrayNode('entities')
+            ->useAttributeAsKey('name')
+            ->prototype('variable')
             ->end()
-
+            ->end()
+            ->end()
+            ->end()
         ;
 
         return $treeBuilder;
